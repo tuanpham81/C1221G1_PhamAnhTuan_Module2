5ec437bd9d2732268e560195af0819cd704501ed
tuanpham81 <tuanpham8196@gmail.com>
2022-02-23 17:10:29 +0700

    commit

--- src/ke_thua/bai_tap/point_vs_moveable_point/MoveablePointTest.java
+++ src/ke_thua/bai_tap/point_vs_moveable_point/MoveablePointTest.java
@@ -3,7 +3,6 @@ package ke_thua.bai_tap.point_vs_moveable_point;
 public class MoveablePointTest {
     public static void main(String[] args) {
         MoveablePoint moveablePoint = new MoveablePoint(1,2,4,5);
-        moveablePoint.move();
-        System.out.println(moveablePoint);
+        System.out.println(moveablePoint.move());
     }
 }
